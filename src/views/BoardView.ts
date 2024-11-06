9018a5e7ce7814717d8b7a65808f75f71de81a69
Birdman1104 <arsenmazmanyan11@gmail.com>
2024-11-06 13:49:03 +0400

    Initial commit.

---
+++ src/views/BoardView.ts
@@ -0,0 +1,12 @@
+import { Container } from 'pixi.js';
+
+export class BoardView extends Container {
+    constructor() {
+        super();
+        this.build();
+    }
+
+    private build(): void {
+        //
+    }
+}
---
+++ webpack/webpack.common.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: ['./src/index.ts'],
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].js',
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$|\.jsx?$/,
+                include: path.join(__dirname, '../src'),
+                loader: 'ts-loader',
+            },
+            // {
+            //   test: /\.css$/,
+            //   use: ["style-loader", "css-loader"],
+            // },
+            // {
+            //   test: /\.(png|jpg|mp3)$/,
+            //   use: [
+            //     {
+            //       loader: "file-loader",
+            //       options: {
+            //         name: "[path][name].[ext]",
+            //       },
+            //     },
+            //   ],
+            // },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            gameName: 'Pixi TypeScript',
+            template: './index.html',
+            filename: 'index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{ from: './src/assets', to: 'assets' }],
+        }),
+    ],
+};
