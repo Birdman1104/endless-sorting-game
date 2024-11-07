e178ab1b7e5b6046bc7e313ebb04cbd33f8a8d9d
Birdman1104 <59249974+Birdman1104@users.noreply.github.com>
2024-11-07 18:10:26 +0000

    Deploying to gh-pages from @ Birdman1104/endless-sorting-game@d146464f6cf33202269a8ab5c2670ce950c66d5e 🚀

---
+++ assets/assetsNames/atlas.ts
@@ -0,0 +1,4 @@
+export const atlases: { name: string; json: string; png: string }[] = [
+    { name: 'elements', json: 'assets/atlas/elements@1.png.json', png: 'assets/atlas/elements.png' },
+    { name: 'game-ui', json: 'assets/atlas/game-ui@1.png.json', png: 'assets/atlas/game-ui.png' },
+];
---
+++ assets/assetsNames/spines.ts
@@ -0,0 +1,14 @@
+export const spines: SpineFiles[] = [
+    {
+        key: 'card',
+        jsonURL: 'assets/spines/card/cards.json',
+        atlasURL: 'assets/spines/card/cards.atlas',
+        preMultipliedAlpha: true,
+    },
+    {
+        key: 'spineboy',
+        jsonURL: 'assets/spines/spineboy/spineboy.json',
+        atlasURL: 'assets/spines/spineboy/spineboy.atlas',
+        preMultipliedAlpha: true,
+    },
+];
