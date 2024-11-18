11698fe6d098ddcfd602aa973c332379fe54c320
Birdman1104 <arsenmazmanyan11@gmail.com>
2024-11-18 12:27:50 +0400

    Added favicon, changed name.

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -39,7 +39,7 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            gameName: 'Pixi TypeScript',
+            gameName: 'Endless Sorting Game',
             template: './index.html',
             filename: 'index.html',
         }),
